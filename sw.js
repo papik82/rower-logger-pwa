72ea867523646309f193ef0ab0c59a2c83d36d24
papik82 <pawel@kroppa.pl>
2026-08-25 21:32:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const SHELL_FILES = [
   "./index.html",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
